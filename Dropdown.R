f02690a66fcd46d6596a3cbb9ecdde7c7449b52b
munozlun <94703981+munozlun@users.noreply.github.com>
2021-11-28 13:07:12 -0600

    Update Dropdown.R

--- Dropdown.R
+++ Dropdown.R
@@ -1,22 +1,26 @@
 library(shiny)
-ui <- fluidPage(
-  selectInput(inputId = "region", label = "Choose a region",
-              c("Select..." = "",
-                "Latin America/Caribbean" = "la",
-                "North America" = "n",
-                "Western Europe" = "w")),
-  radioButtons(inputId = "reg", label = "Choose your region(s)",
-               c("World" = "all",
-                 "Latin America/Caribbean" = "lac",
-                 "North America" = "na",
-                 "Western Europe" = "we"))
-
-)
+ui <- navbarPage("Pursuit of Happiness",
+                 tabPanel("What is Happiness?"),
+                 tabPanel("Exploring the Data"),
+                 tabPanel("A Paradox"),
+                 tabPanel("What is Happiness (seriously)?"),
+                 tabPanel("References"))
+# fluidPage(
+#   selectInput(inputId = "region", label = "Choose a region",
+#               c("Select..." = "",
+#                 "Latin America/Caribbean" = "la",
+#                 "North America" = "n",
+#                 "Western Europe" = "w")),
+#   radioButtons(inputId = "reg", label = "Choose your region(s)",
+#                c("World" = "all",
+#                  "Latin America/Caribbean" = "lac",
+#                  "North America" = "na",
+#                  "Western Europe" = "we"))
+#   
+# )
 
 server <- function(input, output) {}
 
 shinyApp(ui = ui, server = server)
-
-
 
 
