59a7b5b4aebda58a024d4d96eed14b8d5e8feca9
munozlun <94703981+munozlun@users.noreply.github.com>
2021-11-28 13:17:20 -0600

    Update Dropdown.R

--- Dropdown.R
+++ Dropdown.R
@@ -1,10 +1,71 @@
 library(shiny)
 ui <- navbarPage("Pursuit of Happiness",
-                 tabPanel("What is Happiness?"),
-                 tabPanel("Exploring the Data"),
-                 tabPanel("A Paradox"),
-                 tabPanel("What is Happiness (seriously)?"),
-                 tabPanel("References"))
+                 #format to add content to pages is tabPanel(title, content)
+                 tabPanel("What is Happiness?",fluidPage(
+                   selectInput(inputId = "region", label = "Choose a region",
+                               c("Select..." = "",
+                                 "Latin America/Caribbean" = "la",
+                                 "North America" = "n",
+                                 "Western Europe" = "w")),
+                   radioButtons(inputId = "reg", label = "Choose your region(s)",
+                                c("World" = "all",
+                                  "Latin America/Caribbean" = "lac",
+                                  "North America" = "na",
+                                  "Western Europe" = "we"))
+                   
+                 )),
+                 tabPanel("Exploring the Data",fluidPage(
+                   selectInput(inputId = "region", label = "Choose a region",
+                               c("Select..." = "",
+                                 "Latin America/Caribbean" = "la",
+                                 "North America" = "n",
+                                 "Western Europe" = "w")),
+                   radioButtons(inputId = "reg", label = "Choose your region(s)",
+                                c("World" = "all",
+                                  "Latin America/Caribbean" = "lac",
+                                  "North America" = "na",
+                                  "Western Europe" = "we"))
+                   
+                 )),
+                 tabPanel("A Paradox",fluidPage(
+                   selectInput(inputId = "region", label = "Choose a region",
+                               c("Select..." = "",
+                                 "Latin America/Caribbean" = "la",
+                                 "North America" = "n",
+                                 "Western Europe" = "w")),
+                   radioButtons(inputId = "reg", label = "Choose your region(s)",
+                                c("World" = "all",
+                                  "Latin America/Caribbean" = "lac",
+                                  "North America" = "na",
+                                  "Western Europe" = "we"))
+                   
+                 )),
+                 tabPanel("What is Happiness (seriously)?",fluidPage(
+                   selectInput(inputId = "region", label = "Choose a region",
+                               c("Select..." = "",
+                                 "Latin America/Caribbean" = "la",
+                                 "North America" = "n",
+                                 "Western Europe" = "w")),
+                   radioButtons(inputId = "reg", label = "Choose your region(s)",
+                                c("World" = "all",
+                                  "Latin America/Caribbean" = "lac",
+                                  "North America" = "na",
+                                  "Western Europe" = "we"))
+                   
+                 )),
+                 tabPanel("References",fluidPage(
+                   selectInput(inputId = "region", label = "Choose a region",
+                               c("Select..." = "",
+                                 "Latin America/Caribbean" = "la",
+                                 "North America" = "n",
+                                 "Western Europe" = "w")),
+                   radioButtons(inputId = "reg", label = "Choose your region(s)",
+                                c("World" = "all",
+                                  "Latin America/Caribbean" = "lac",
+                                  "North America" = "na",
+                                  "Western Europe" = "we"))
+                   
+                 )))
 # fluidPage(
 #   selectInput(inputId = "region", label = "Choose a region",
 #               c("Select..." = "",
@@ -16,11 +77,12 @@ ui <- navbarPage("Pursuit of Happiness",
 #                  "Latin America/Caribbean" = "lac",
 #                  "North America" = "na",
 #                  "Western Europe" = "we"))
-#   
+# 
 # )
 
 server <- function(input, output) {}
 
 shinyApp(ui = ui, server = server)
+
 
 
