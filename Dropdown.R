467608aa249db6917ade242446663bc43678b208
munozlun <94703981+munozlun@users.noreply.github.com>
2021-12-03 10:45:28 -0600

    Update Dropdown.R

--- Dropdown.R
+++ Dropdown.R
@@ -1,44 +1,44 @@
 library(shiny)
 library(ggplot2)
 library(plotly)
-ui <- navbarPage("Pursuit of Happiness",
+ui <- navbarPage("Pursuit of Happiness", collapsible = TRUE, inverse = TRUE, fluid = TRUE, 
                  tabPanel("What is Happiness?",h1("What is Happiness?"),"Explain what the WHR does and how they define happiness"),
                  tabPanel("Exploring the Data",h1("Exploring the Data"),fluidPage(
                    mainPanel(
                      selectInput(inputId = "region", label = "Choose a region",
-                               c("Select..." = "",
-                                 "Latin America and Caribbean" = "la",
-                                 "North America and ANZ" = "no",
-                                 "Western Europe" = "we",
-                                 "Central and Eastern Europe" = "ce",
-                                 "Middle East and North Africa" = "mi",
-                                 "Sub-Saharan Africa" = "su",
-                                 "East Asia" = "ea",
-                                 "Southeast Asia" = "so",
-                                 "Commonwealth of Independent States" = "co",
-                                 "World" = "wo"))
-                                ,
-                   radioButtons(inputId = "reg", label = "Choose your region(s)",
-                                c("Latin America and Caribbean" = "la",
-                                  "North America and ANZ" = "no",
-                                  "Western Europe" = "we",
-                                  "Central and Eastern Europe" = "ce",
-                                  "Middle East and North Africa" = "mi",
-                                  "Sub-Saharan Africa" = "su",
-                                  "East Asia" = "ea",
-                                  "Southeast Asia" = "so",
-                                  "Commonwealth of Independent States" = "co",
-                                  "World" = "wo")),
-                   
-                   checkboxGroupInput("varSelect", "Choose Measures of Happiness",
-                                      c("GDP per Capita",
-                                      "Generosity",
-                                      "Perceived Corruption",
-                                      "Social Support",
-                                      "Life Expectancy",
-                                      "Free Will"))
-                 
-                 #  img(src = 'quokka.png', align = "right"),
+                                 c("Select..." = "",
+                                   "Latin America and Caribbean" = "la",
+                                   "North America and ANZ" = "no",
+                                   "Western Europe" = "we",
+                                   "Central and Eastern Europe" = "ce",
+                                   "Middle East and North Africa" = "mi",
+                                   "Sub-Saharan Africa" = "su",
+                                   "East Asia" = "ea",
+                                   "Southeast Asia" = "so",
+                                   "Commonwealth of Independent States" = "co",
+                                   "World" = "wo"))
+                     ,
+                     radioButtons(inputId = "reg", label = "Choose your region(s)",
+                                  c("Latin America and Caribbean" = "la",
+                                    "North America and ANZ" = "no",
+                                    "Western Europe" = "we",
+                                    "Central and Eastern Europe" = "ce",
+                                    "Middle East and North Africa" = "mi",
+                                    "Sub-Saharan Africa" = "su",
+                                    "East Asia" = "ea",
+                                    "Southeast Asia" = "so",
+                                    "Commonwealth of Independent States" = "co",
+                                    "World" = "wo")),
+                     
+                     checkboxGroupInput("varSelect", "Choose Measures of Happiness",
+                                        c("GDP per Capita",
+                                          "Generosity",
+                                          "Perceived Corruption",
+                                          "Social Support",
+                                          "Life Expectancy",
+                                          "Free Will"))
+                     
+                     #  img(src = 'quokka.png', align = "right"),
                    ))),
                  tabPanel("A Paradox",h1("A Paradox"),"Explain disparity between 'happiness' levels and 'depression' levels"),
                  tabPanel("What is Happiness (seriously)?","Explain our own modified way to calculate happiness",h1("What is Happiness (seriously)?"),fluidPage(
